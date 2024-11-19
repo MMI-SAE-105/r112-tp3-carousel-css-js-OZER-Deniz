e38ff8b0db156a21a2e4d7a327fa484fc1eb2d98
OZER-Deniz <deniz.ozer@edu.univ-fcomte.fr>
2024-11-19 15:20:55 +0100

    défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 // *** CAROUSEL ***
 // TODO
-const carousel = document.querySelector("carousel__container")
-const prevButton = document.querySelector("carousel__button--prev")
-const nextButton = document.querySelector("carousel__button--next")
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
 
 if (carousel) {
 
@@ -16,6 +16,7 @@ nextButton.addEventListener("click", () => {
     scrollBy({
         left: +260, behavior : "smooth"
     })});
-}
+};
 
-
+const premierItem = document.querySelector(".carousel__item");
+scrollAmout = 981;
