e9e6a4af6fe3e2812506ecfc60e55d02883c48d2
OZER-Deniz <deniz.ozer@edu.univ-fcomte.fr>
2024-11-19 15:14:29 +0100

    défilement de base précédent/suivant

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,21 @@
 // *** CAROUSEL ***
 // TODO
+const carousel = document.querySelector("carousel__container")
+const prevButton = document.querySelector("carousel__button--prev")
+const nextButton = document.querySelector("carousel__button--next")
+
+if (carousel) {
+
+prevButton.addEventListener("click", () => {
+    scrollBy({ 
+        left: +260, behavior: "smooth" 
+    })});
+
+
+nextButton.addEventListener("click", () => {
+    scrollBy({
+        left: +260, behavior : "smooth"
+    })});
+}
+
+
